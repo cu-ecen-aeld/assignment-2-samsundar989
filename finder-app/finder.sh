f817b701a434c3c9373b80c987fd36a678d38799
samsu <samsu@Ubuntu.myguest.virtualbox.org>
2023-09-21 01:33:28 -0400

    init

---
+++ finder-app/finder.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+filesdir=$1
+searchstr=$2
+
+if [ $# -eq 2]; then
+    echo "Correct number of arguments, continuing..."
+else
+    echo "ERROR: Invalid number of arguments, program requires two arguments: file directory path and string to be searched, in that order"
+    exit 1
+fi
+
+if [ -d ${filesdir} ]; then
+    echo "Directory exists, continuing..."
+else
+    echo "ERROR: Directory not found, enter a directory that exists. Exiting... "
+    exit 1
+fi
+
+
+
